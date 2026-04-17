9749ce9991f3e3dd4eada2778dc7054c66124bab
bipulkumar16 <bipul.kumar160290@gmail.com>
2026-04-17 12:03:47 +0530

    Add: remove-duplicates-from-sorted-array - submission-1

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        HashSet<Integer> set = new LinkedHashSet<>();
+        for(int num : nums){
+            set.add(num);
+        }
+        int i = 0;
+    for (int num : set) {
+        nums[i++] = num;
+    }
+    return set.size();
+        
+    }
+}
