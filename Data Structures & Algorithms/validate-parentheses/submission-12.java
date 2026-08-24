22685359ba1a8d15785fca87e102dea65afcbdd0
bipulkumar16 <bipul.kumar160290@gmail.com>
2026-08-24 08:14:09 +0530

    Add: validate-parentheses - submission-12

---
+++ Data Structures & Algorithms/validate-parentheses/submission-12.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isValid(String s) {
+        HashMap<Character, Character> map = new HashMap<>();
+        map.put(')', '(');
+        map.put('}', '{');
+        map.put(']', '[');
+        Stack<Character> stack = new Stack<>();
+        for(char ch : s.toCharArray()){
+            if(map.containsValue(ch)) stack.push(ch);
+            else if(stack.isEmpty() || stack.pop() != map.get(ch)) return false;
+        }
+        return stack.isEmpty();
+    }
+}
