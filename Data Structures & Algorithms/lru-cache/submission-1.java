0a61d128baae92f0b0b9af20e62769392b07a00d
bipulkumar16 <bipul.kumar160290@gmail.com>
2026-09-04 07:54:10 +0530

    Add: lru-cache - submission-1

---
+++ Data Structures & Algorithms/lru-cache/submission-1.java
@@ -0,0 +1,94 @@
+class LRUCache {
+
+    class Node {
+        int key;
+        int value;
+        Node prev;
+        Node next;
+
+        Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private final int capacity;
+    private final Map<Integer, Node> map;
+
+    private final Node head;
+    private final Node tail;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.map = new HashMap<>();
+
+        head = new Node(0, 0);
+        tail = new Node(0, 0);
+
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public int get(int key) {
+
+        if (!map.containsKey(key)) {
+            return -1;
+        }
+
+        Node node = map.get(key);
+
+        remove(node);
+        addToFront(node);
+
+        return node.value;
+    }
+
+    public void put(int key, int value) {
+
+        // Key already exists
+        if (map.containsKey(key)) {
+
+            Node node = map.get(key);
+
+            remove(node);
+
+            node.value = value;
+
+            addToFront(node);
+
+            return;
+        }
+
+        // New key
+        Node node = new Node(key, value);
+
+        map.put(key, node);
+
+        addToFront(node);
+
+        // Cache exceeded capacity
+        if (map.size() > capacity) {
+
+            Node lru = tail.prev;
+
+            remove(lru);
+
+            map.remove(lru.key);
+        }
+    }
+
+    private void remove(Node node) {
+
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    private void addToFront(Node node) {
+
+        node.next = head.next;
+        node.prev = head;
+
+        head.next.prev = node;
+        head.next = node;
+    }
+}
